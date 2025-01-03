ad4f349ecc8c99c71a849c23919fbe75703d054f
Alex Leung <alexwkleung25@gmail.com>
2025-01-02 21:40:00 -0800

    Add start/end date query params

--- src/components/DateRangePicker/DateRangePicker.tsx
+++ src/components/DateRangePicker/DateRangePicker.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { DatePicker } from '@mui/x-date-pickers';
 import { Box } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -5,9 +6,20 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { useDateRange } from '../../hooks/useDateRange';
 import dayjs from 'dayjs';
 import { DATE_FORMAT } from '../../constants/date';
+import { useSearchParams } from 'react-router';
 
 const DateRangePicker = () => {
   const { date, setDate } = useDateRange();
+
+  const [, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    // update search params based on start/end date values
+    setSearchParams({
+      startDate: date.startDate,
+      endDate: date.endDate,
+    });
+  }, [date.startDate, date.endDate, setSearchParams]);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
